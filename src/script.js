ee88e5471546704415d172258465305b8095fdb6
Igor Shavlovsky <shahmanul@34gmail.com>
2022-12-08 12:23:29 +0300

    Убрал разметку таблицы; Скрыл кнопку «очистить»; Переделал логику обновления графика; Убрал consol.log; Добавил валидацию на кнопку «расчитать» Добавил класс «button:disabled»; Добавил «background-color: transparent;» для полоски слайдера;

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
-import './style.css'
+// import '../src/style.css'
+import '../src/scss/main.scss'
+
 import {Chart, registerables} from 'chart.js';
 
 const totalBtn = document.getElementById('total')
@@ -15,7 +17,7 @@ const interest_rate_input = document.getElementById('interest_rate')
 const time_input = document.getElementById('time')
 const time_range = document.getElementById('time_range')
 
-const table = document.getElementById('table')
+// const table = document.getElementById('table')
 
 const inputArr = [first_payment_input, first_payment_range, monthly_payment_input, monthly_payment_range, time_input, time_range]
 
@@ -32,39 +34,67 @@ const myChart = new Chart(ctx, {
         labels: [],
         datasets: [
             {
-                label: ["Начальная сумма"],
+                label: ["начальная сумма"],
                 data: [],
                 backgroundColor: "#10273D",
-                borderColor: "#10273D",
+                // borderColor: "#10273D",
             },
             {
-                label: ["Пополнения"],
+                label: ["пополнения"],
                 data: [],
                 backgroundColor: "#0D6579",
-                borderColor: "#0D6579",
+                // borderColor: "#0D6579",
             },
             {
-                label: ["Начисленные проценты"],
+                label: ["начисленные проценты"],
                 data: [],
                 backgroundColor: "#04CCD9",
-                borderColor: "#04CCD9",
+                // borderColor: "#04CCD9",
             }
         ]
     },
     options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'bottom',
+                useBorderRadius: true,
+                borderRadius: 10,
+                labels: {
+                    color: 'rgba(16, 39, 61, 0.5)'
+                }
+            }
+        },
+        transitions: {
+            show: {
+                animations: {
+                    tension: {
+                        duration: 800,
+                        easing: 'linear',
+                        from: 0,
+                        to: 1,
+                        loop: true
+                    }
+                }
+            }
+        },
         scales: {
             x: {
-                stacked: true
+                stacked: true,
+                display:false
             },
             y: {
                 stacked: true,
-                suggestedMin: 10000,
-                suggestedMax: 100000
+                ticks: {
+                    callback: function(value) {
+                        return convertToRub(value.toFixed());
+                    }
+                }
             }
         }
     }
 });
-
 
 first_payment_input.value = 1000000
 monthly_payment_input.value = 50000
@@ -77,6 +107,8 @@ let totalArr = []
 let profitArr = []
 let sumProfitArr = []
 let monthlyPaymentArr = []
+let labelsArr = []
+let firstPaymentArr = []
 
 function getProfit(f, m, i, p) {
     profit = Number(((f + m) * i) / p)
@@ -88,20 +120,21 @@ function clearResult(chart) {
         profitArr = []
         sumProfitArr = []
         monthlyPaymentArr = []
-        addTotals([0.00, 0.00], [0.00, 0.00], [0.00, 0.00])
+        addTotals([0, 0], [0, 0], [0, 0])
         for (let i = 0; i < 3; i++) {
             myChart.data.datasets[i].data = []
             myChart.data.labels = []
         }
         chart.update();
-        table.innerHTML = ``
+        // table.innerHTML = ``
     }
 }
 
 function addTotals(t_sum, t_payment, t_percent) {
-    total_sum.innerText = `${convertToRub(t_sum.at(-1).toFixed(2))}`
-    total_payment.innerText = `${convertToRub((t_payment.at(-1) + t_sum[0]).toFixed(2))}`
-    total_percent.innerText = `${convertToRub(t_percent.at(-1).toFixed(2))}`
+    let sum = t_payment.pop() + t_sum[0]
+    total_sum.innerText = `${convertToRub(t_sum.pop())}`
+    total_payment.innerText = `${convertToRub(sum)}`
+    total_percent.innerText = `${convertToRub(t_percent.pop())}`
 }
 
 function addDataChart(labels, data1, data2, data3) {
@@ -109,7 +142,11 @@ function addDataChart(labels, data1, data2, data3) {
     myChart.data.datasets[1].data.push(data2)
     myChart.data.datasets[2].data.push(data3)
     myChart.data.labels.push(`${labels}`)
-    myChart.update();
+
+    if(myChart.data.labels.length >= Number(time_input.value)) {
+        myChart.update();
+    }
+
 }
 
 function convertToRub(n) {
@@ -117,7 +154,9 @@ function convertToRub(n) {
         'ru-RU',
         {
             style: 'currency',
-            currency: 'RUB'
+            currency: 'RUB',
+            maximumFractionDigits: 0,
+            minimumFractionDigits: 0,
         }
     ).format(n)
 }
@@ -134,20 +173,22 @@ function addData(data, mp, pr, fp) {
             let sumPercent = sumProfitArr[i] + profitArr[i + 1]
             sumProfitArr.push(sumPercent)
         }
+        firstPaymentArr.push(fp)
 
+        labelsArr.push(i + 1)
         addDataChart(i + 1, fp, sumMP, sumProfitArr[i])
 
 
-        table.innerHTML +=
-            `<tr>
-                <td>${i + 1}</td>
-                <td>${convertToRub(data[i].toFixed(2))}</td>
-                <td>${convertToRub(mp.toFixed(2))}</td>
-                <td>${convertToRub(sumMP.toFixed(2))}</td>
-                <td>${convertToRub(percent.toFixed(2))}</td>
-                <td>${convertToRub(sumProfitArr[i].toFixed(2))}</td>
-                <td>${convertToRub(totals.toFixed(2))}</td>
-            </tr>`
+        // table.innerHTML +=
+        //     `<tr>
+        //         <td>${i + 1}</td>
+        //         <td>${convertToRub(data[i].toFixed(2))}</td>
+        //         <td>${convertToRub(mp.toFixed(2))}</td>
+        //         <td>${convertToRub(sumMP.toFixed(2))}</td>
+        //         <td>${convertToRub(percent.toFixed(2))}</td>
+        //         <td>${convertToRub(sumProfitArr[i].toFixed(2))}</td>
+        //         <td>${convertToRub(totals.toFixed(2))}</td>
+        //     </tr>`
     }
     // console.log(totalArr)
     // console.log(profitArr)
@@ -176,9 +217,10 @@ function getTotal() {
     const even = (element) => element;
 
     if (validatorArr.every(even)) {
-        console.log(validatorArr.every(even))
+        // console.log(validatorArr.every(even))
         inputsArr.forEach(el => {
             el.classList.remove('error')
+            totalBtn.disabled = false
         })
         totalArr.push(first_payment)
 
@@ -204,6 +246,7 @@ function getTotal() {
         validatorArr.forEach((el, i) => {
             if (!el) {
                 inputsArr[i].classList.add('error')
+                totalBtn.disabled = true
             }
         })
     }
@@ -214,27 +257,33 @@ function rangeLine() {
         e.style.setProperty('--value', e.value);
         e.style.setProperty('--min', e.min === '' ? '0' : e.min);
         e.style.setProperty('--max', e.max === '' ? '100' : e.max);
+        e.style.setProperty('--value', e.value)
         e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+        e.addEventListener('change', () => e.style.setProperty('--value', e.value));
     }
 }
 
 const inputValue = function (event) {
+    rangeLine()
     if (event.target.type === "range") {
-        rangeLine()
         let firstLetter = event.target.id.slice(0, 1)
         let numbInput = document.querySelectorAll('input[type=number]')
         numbInput.forEach(inputId => {
             if (firstLetter === inputId.id.slice(0, 1)) {
                 inputId.value = event.target.value
                 inputId.classList.remove('error')
+                totalBtn.disabled = false
             }
         })
     } else if (event.target.type === "number") {
-        rangeLine()
-        if (event.target.value === '' || Number(event.target.value) < Number(event.target.min) || Number(event.target.value) > Number(event.target.max)) {
+        if (event.target.value === '' ||
+            Number(event.target.value) < Number(event.target.min) ||
+            Number(event.target.value) > Number(event.target.max)) {
             event.target.classList.add('error')
+            totalBtn.disabled = true
         } else {
             event.target.classList.remove('error')
+            totalBtn.disabled = false
             let firstLetter = event.target.id.slice(0, 1)
             let numbInput = document.querySelectorAll('input[type=range]')
             numbInput.forEach(inputId => {
